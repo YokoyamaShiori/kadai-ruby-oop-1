4a84967f987d3b687a8e7f098dca9b06b5d11369
shiori.yokoyama <b.dry.doll@gmail.com>
2021-07-04 08:10:17 +0000

    third commit

--- team.rb
+++ team.rb
@@ -5,8 +5,8 @@ class Team
   attr_accessor :name, :win, :lose, :draw
 
   # インスタンスを初期化するための、特別なメソッド
-  def initialize(suffix)
-    self.name = suffix 
+  def initialize(name,win,lose,draw)
+    self.name = name
     self.win = win
     self.lose = lose
     self.draw = draw
@@ -23,30 +23,12 @@ class Team
 end
 
 # インスタンスの生成と、変数への代入
-team1 = Team.new("Giants")
-team1.win = 67
-team1.lose = 45
-team1.draw = 8
-team2 = Team.new("Tigers")
-team2.win = 60
-team2.lose = 53
-team2.draw = 7
-team3 = Team.new("Dragons")
-team3.win = 60
-team3.lose = 55
-team3.draw = 5
-team4 = Team.new("BayStars")
-team4.win = 56
-team4.lose = 58
-team4.draw = 6
-team5 = Team.new("Carp")
-team5.win = 52
-team5.lose = 56
-team5.draw = 12
-team6 = Team.new("Swallows")
-team6.win = 41
-team6.lose = 69
-team6.draw = 10
+team1 = Team.new("Giants",67,45,8)
+team2 = Team.new("Tigers",60,53,7)
+team3 = Team.new("Dragons",60,55,5)
+team4 = Team.new("BayStars",56,58,6)
+team5 = Team.new("Carp",52,56,12)
+team6 = Team.new("Swallows",41,69,10)
 
 # インスタンスの使用
 team1.show_team_result
